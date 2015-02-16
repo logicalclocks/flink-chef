72762afd9174b0db640048a53ec6e20d654128d9
Jim Dowling <jdowling@kth.se>
2015-02-17 00:27:30 +0100

    metadata updates

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0"
 
 recipe           "install", "Installs Apache Flink"
-recipe           "example", "Runs wordcoutn example"
+recipe           "master", "Starts a Flink JobManager in standalone mode"
+recipe           "slave", "Starts a Flink Slave in standalone mode"
 
 depends          "hadoop"
 depends          "hopagent"
