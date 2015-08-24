6d2d73ce94c6088578a50e4b0156553e7180b25f
Jim Dowling <jdowling@kth.se>
2015-08-24 23:29:42 +0200

    Add ssh_config to taskmanagers and the jobmanager

--- providers/jobmanager.rb
+++ providers/jobmanager.rb
@@ -5,36 +5,47 @@ action :return_publickey do
  node.default[:flink][:jobmanager][:public_key] = "#{contents}"
 
 # This works for chef-solo - we are executing this recipe.rb file.
-recipeName = "#{__FILE__}".gsub(/.*\//, "")
-recipeName = "#{recipeName}".gsub(/\.rb/, "")
- 
-kagent_param "/tmp" do
-  executing_cookbook "flink"
-  executing_recipe  "jobmanager"
-  cookbook "flink"
-  recipe "jobmanager"
-  param "public_key"
-  value  node[:flink][:jobmanager][:public_key]
-end
+ recipeName = "#{__FILE__}".gsub(/.*\//, "")
+ recipeName = "#{recipeName}".gsub(/\.rb/, "")
 
+ Chef::Log.info "Recipe name is #{recipeName}"
+
+  template "#{homedir}/.ssh/config" do
+    source "ssh_config.erb"
+    owner node[:flink][:user]
+    group node[:flink][:group]
+    mode 0664
+  end
+ 
+ kagent_param "/tmp" do
+   executing_cookbook "flink"
+   executing_recipe  "jobmanager"
+   cookbook "flink"
+   recipe "jobmanager"
+   param "public_key"
+   value  node[:flink][:jobmanager][:public_key]
+ end
 end
 
 action :get_publickey do
+  homedir = "#{new_resource.homedir}"
 
-homedir = "#{new_resource.homedir}"
-
-Chef::Log.info "JobMgr public key read is: #{node[:flink][:jobmanager][:public_key]}"
-
-bash "add_jobmgr_public_key" do
- user node[:flink][:user]
- group node[:flink][:group]
- code <<-EOF
+  template "#{homedir}/.ssh/config" do
+    source "ssh_config.erb"
+    owner node[:flink][:user]
+    group node[:flink][:group]
+    mode 0664
+  end
+  
+  Chef::Log.info "JobMgr public key read is: #{node[:flink][:jobmanager][:public_key]}"
+  bash "add_jobmgr_public_key" do
+    user node[:flink][:user]
+    group node[:flink][:group]
+    code <<-EOF
       mkdir #{homedir}/.ssh
       echo "#{node[:flink][:jobmanager][:public_key]}" >> #{homedir}/.ssh/authorized_keys
       touch #{homedir}/.ssh/.jobmgr_key_authorized
   EOF
- not_if { ::File.exists?( "#{homedir}/.ssh/.jobmgr_key_authorized" || "#{node[:flink][:jobmanager][:public_key]}".empty? ) }
-end
-
-
+    not_if { ::File.exists?( "#{homedir}/.ssh/.jobmgr_key_authorized" || "#{node[:flink][:jobmanager][:public_key]}".empty? ) }
+  end
 end
