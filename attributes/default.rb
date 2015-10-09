bcbdcd0cb3f2517e7020eb909dd700e284df8321
Jim Dowling <jdowling@sics.se>
2015-10-09 15:57:51 +0200

    back to 0.9.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ include_attribute "hadoop"
 default[:flink][:user]                             = "flink"
 default[:flink][:group]                            = "hadoop"
 
-default[:flink][:version]                          = "0.9.0" 
+default[:flink][:version]                          = "0.9.1" 
 
 default[:flink][:hadoop_version]                   = "24"
 default[:flink][:dir]                              = "/usr/local"
