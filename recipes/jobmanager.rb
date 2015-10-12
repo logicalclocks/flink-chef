a0340948013db1d49a93a45b358aad94bd48640d
Jim Dowling <jdowling@sics.se>
2015-10-12 22:40:20 +0200

    disable hdfs dirs

--- recipes/jobmanager.rb
+++ recipes/jobmanager.rb
@@ -35,21 +35,21 @@ template "/etc/init.d/jobmanager" do
   notifies :restart, resources(:service => "jobmanager"), :immediately
 end
 
-hadoop_hdfs_directory "/User/#{node[:flink][:user]}" do
-#  action :create_as_superuser
-  action :create
-  owner node[:flink][:user]
-  group node[:flink][:group]
-  mode "1775"
-end
+# hadoop_hdfs_directory "/User/#{node[:flink][:user]}" do
+# #  action :create_as_superuser
+#   action :create
+#   owner node[:flink][:user]
+#   group node[:flink][:group]
+#   mode "1775"
+# end
 
-hadoop_hdfs_directory "/User/#{node[:flink][:user]}/checkpoints" do
-#  action :create_as_superuser
-  action :create
-  owner node[:flink][:user]
-  group node[:flink][:group]
-  mode "1775"
-end
+# hadoop_hdfs_directory "/User/#{node[:flink][:user]}/checkpoints" do
+# #  action :create_as_superuser
+#   action :create
+#   owner node[:flink][:user]
+#   group node[:flink][:group]
+#   mode "1775"
+# end
 
 
 homedir = node[:flink][:user].eql?("root") ? "/root" : "/home/#{node[:flink][:user]}"
