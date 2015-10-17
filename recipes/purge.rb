ca201c2c36b60e6f393bafea2b99477b9949d510
Jim Dowling <jdowling@sics.se>
2015-10-18 00:20:43 +0200

    purge added

---
+++ recipes/purge.rb
@@ -0,0 +1,22 @@
+bash 'kill_running_service_flink' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      pkill JobManager
+      pkill TaskManager
+    EOF
+end
+
+
+directory node[:flink][:home] do
+  recursive true
+  action :delete
+  ignore_failure :true
+end
+
+link node[:flink][:dir] + "/flink" do
+  action :delete
+  ignore_failure :true
+end
+
+
