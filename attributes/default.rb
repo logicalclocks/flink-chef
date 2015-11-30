9441c33dce822407d27e3ab0811bd91a748166aa
Jim Dowling <jdowling@sics.se>
2015-11-30 13:28:55 +0100

    add scala version to url

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,9 +9,10 @@ default[:flink][:group]                            = "hadoop"
 default[:flink][:version]                          = "0.10.1" 
 
 default[:flink][:hadoop_version]                   = "24"
+default[:flink][:scala_version]                    = "2.10"
 default[:flink][:dir]                              = "/usr/local"
 default[:flink][:home]                             = "#{node[:flink][:dir]}/flink-#{node[:flink][:version]}"
-default[:flink][:url]                              = "#{download_url}/flink-#{node[:flink][:version]}-bin-hadoop#{node[:flink][:hadoop_version]}.tgz"
+default[:flink][:url]                              = "#{download_url}/flink-#{node[:flink][:version]}-bin-hadoop#{node[:flink][:hadoop_version]}-scala_#{node[:flink][:scala_version]}.tgz"
 default[:flink][:conf_dir]                         = "#{node[:flink][:home]}/conf"
 
 default[:flink][:checksum]                         = ""
