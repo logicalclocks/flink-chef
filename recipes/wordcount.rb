a1636c70ed5178f1096d59d8035e07093a9e11a0
Jim Dowling <jdowling@sics.se>
2016-01-23 09:35:57 +0100

    User -> user for hdfs

--- recipes/wordcount.rb
+++ recipes/wordcount.rb
@@ -9,7 +9,7 @@ end
 
 hadoop_hdfs_directory "#{Chef::Config[:file_cache_path]}/apache.txt" do
   action :put
-  dest "/User/#{node[:flink][:user]}"
+  dest "/user/#{node[:flink][:user]}"
   owner node[:flink][:user]
   mode "775"
 end
@@ -23,6 +23,6 @@ nn="#{master_ip}:#{node[:hadoop][:nn][:port]}"
      cd #{node[:flink][:home]}
      # This generates about 100 MB of random data, takes about 30 seconds on my laptop
      # head -c 100000000 /dev/urandom >> examples/dummy.txt
-     echo "./bin/flink run -p 1 -j ./examples/WordCount.jar hdfs://#{nn}/User/#{node[:flink][:user]}/apache.txt hdfs://#{nn}/User/#{node[:flink][:user]}/wordcount-result.txt\n" > README.wordcount
+     echo "./bin/flink run -p 1 -j ./examples/WordCount.jar hdfs://#{nn}/user/#{node[:flink][:user]}/apache.txt hdfs://#{nn}/user/#{node[:flink][:user]}/wordcount-result.txt\n" > README.wordcount
   EOH
  end
