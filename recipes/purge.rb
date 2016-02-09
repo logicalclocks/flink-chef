a570fad87c03cd01f9a86c4bf9b24b21b1e1330a
%github_username% <jdowling@sics.se>
2016-02-09 13:25:54 +0100

    Fixed type bug in purge recipe

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -1,6 +1,6 @@
 bash 'kill_running_service_flink' do
     user "root"
-    ignore_failure :true
+    ignore_failure true
     code <<-EOF
       pkill JobManager
       pkill TaskManager
@@ -11,12 +11,12 @@ end
 directory node[:flink][:home] do
   recursive true
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
 link node[:flink][:dir] + "/flink" do
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
 
