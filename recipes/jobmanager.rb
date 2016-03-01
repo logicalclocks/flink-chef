e25557853e24854e24ab6233cc2d8e144dbeedba
%github_username% <jdowling@sics.se>
2016-03-01 17:55:43 +0100

    dot notation and apache_hadoop udpates

--- recipes/jobmanager.rb
+++ recipes/jobmanager.rb
@@ -1,18 +1,18 @@
 include_recipe "flink::default"
 
-file "#{node[:flink][:conf_dir]}/slaves" do
-   owner node[:flink][:user]
+file "#{node.flink.conf_dir}/slaves" do
+   owner node.flink.user
    action :delete
 end
 
-slaves = node[:flink][:taskmanager][:private_ips].join("\n")
+slaves = node.flink.taskmanager.private_ips.join("\n")
 slaves += "\n"
 
 # Default behaviour of attribute "content" is to replace the contents of
 # the existing file as long as the new contents have a non-default value.
-file "#{node[:flink][:conf_dir]}/slaves" do
-  owner node[:flink][:user]
-  group node[:flink][:group]
+file "#{node.flink.conf_dir}/slaves" do
+  owner node.flink.user
+  group node.flink.group
   mode '644'
   content slaves.to_s
   action :create
@@ -25,28 +25,33 @@ end
 
 template "/etc/init.d/jobmanager" do
   source "jobmanager.erb"
-  owner node[:flink][:user]
-  group node[:flink][:group]
+  owner node.flink.user
+  group node.flink.group
   mode 0754
   variables({
-              :flavor => "#{node[:flink][:mode]}"
+              :flavor => "#{node.flink.mode}"
             })
   notifies :enable, resources(:service => "jobmanager")
   notifies :restart, resources(:service => "jobmanager"), :immediately
 end
 
 
-homedir = node[:flink][:user].eql?("root") ? "/root" : "/home/#{node[:flink][:user]}"
+homedir = node.flink.user.eql?("root") ? "/root" : "/home/#{node.flink.user}"
+
+#
+# Enables the taskmanagers to be started/stopped remotely from the jobmanager by
+# adding the jobmanager's public ssh key to the .ssh/authorized_keys file for each taskmanager
+#
 
 kagent_keys "#{homedir}" do
-  cb_user node[:flink][:user]
-  cb_group node[:flink][:group]
+  cb_user node.flink.user
+  cb_group node.flink.group
   action :generate  
 end  
 
 kagent_keys "#{homedir}" do
-  cb_user node[:flink][:user]
-  cb_group node[:flink][:group]
+  cb_user node.flink.user
+  cb_group node.flink.group
   cb_name "flink"
   cb_recipe "jobmanager"  
   action :return_publickey
--- recipes/wordcount.rb
+++ recipes/wordcount.rb
@@ -1,28 +1,28 @@
 libpath = File.expand_path '../../../kagent/libraries', __FILE__
 
-master_ip = private_recipe_ip("hadoop","nn")
+master_ip = private_recipe_ip("apache_hadoop","nn")
 
 
-remote_file "#{Chef::Config[:file_cache_path]}/apache.txt" do
+remote_file "#{Chef::Config.file_cache_path}/apache.txt" do
   source "https://www.apache.org/licenses/LICENSE-2.0.txt"
 end
 
-hadoop_hdfs_directory "#{Chef::Config[:file_cache_path]}/apache.txt" do
+apache_hadoop_hdfs_directory "#{Chef::Config.file_cache_path}/apache.txt" do
   action :put
-  dest "/user/#{node[:flink][:user]}"
-  owner node[:flink][:user]
+  dest "/user/#{node.flink.user}"
+  owner node.flink.user
   mode "775"
 end
 
-nn="#{master_ip}:#{node[:hadoop][:nn][:port]}"
+nn="#{master_ip}:#{node.apache_hadoop.nn.port}"
 
  bash 'wordcount-example' do
-  user node[:flink][:user]
+  user node.flink.user
   code <<-EOH
      set -e && set -o pipefail
-     cd #{node[:flink][:home]}
+     cd #{node.flink.home}
      # This generates about 100 MB of random data, takes about 30 seconds on my laptop
      # head -c 100000000 /dev/urandom >> examples/dummy.txt
-     echo "./bin/flink run -p 1 -j ./examples/WordCount.jar hdfs://#{nn}/user/#{node[:flink][:user]}/apache.txt hdfs://#{nn}/user/#{node[:flink][:user]}/wordcount-result.txt\n" > README.wordcount
+     echo "./bin/flink run -p 1 -j ./examples/WordCount.jar hdfs://#{nn}/user/#{node.flink.user}/apache.txt hdfs://#{nn}/user/#{node.flink.user}/wordcount-result.txt\n" > README.wordcount
   EOH
  end
