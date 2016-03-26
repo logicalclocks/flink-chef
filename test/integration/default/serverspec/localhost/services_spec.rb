62bfea550db84df27bb6197a0b8229cb053f7f8e
%github_username% <jdowling@sics.se>
2016-03-26 22:27:23 +0100

    test is for yarn

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -11,13 +11,13 @@ describe service('datanode') do
   it { should be_running   }
 end 
 
-describe service('jobmanager') do  
-  it { should be_running   }
-end 
+# describe service('jobmanager') do  
+#   it { should be_running   }
+# end 
 
-describe service('taskmanager') do  
-  it { should be_running   }
-end 
+# describe service('taskmanager') do  
+#   it { should be_running   }
+# end 
 
 describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /user\"") do
   its (:stdout) { should match /flink/ }
